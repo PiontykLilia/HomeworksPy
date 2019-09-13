f3c1440995ef0d1ea2ac74530cb0af0ac4a181a5
PiontykLilia <47629875+PiontykLilia@users.noreply.github.com>
2019-09-13 14:35:49 +0300

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,12 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+#Поміняти між собою значення двох змінних, не використовуючи третьої змінної.
+>>> a=3
+>>> b=5
+>>> a,b=b,a
+>>> print(a)
+5
+>>> print(b)
+3
+>>> 
