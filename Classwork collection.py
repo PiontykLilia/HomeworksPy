78222999b3ab046303c151278d7a95f9ce18d8dd
PiontykLilia <47629875+PiontykLilia@users.noreply.github.com>
2019-10-17 15:54:42 +0300

    Add files via upload

---
+++ Classwork collection.py
@@ -0,0 +1,39 @@
+# nums = []
+# k=int(input("Please enter the count of the elements of sequence: "))
+# for i in range(k):
+#      n = int(input("Please enter the element: "))
+#      nums.append(n)
+# print(nums)
+# max = nums[0]
+# min = nums[0]
+# for i in range(k):
+#     if nums[i] > max:
+#         max = nums[i]
+#     if nums[i] < min:
+#         min = nums[i]
+# # print("Maximum number = %d. Minimum number = %d." %(max, min))
+# print("Maximum number = {}. Minimum number = {}.".format(max, min))
+
+
+# for x in range(1,11):
+#     if x % 2 == 0:
+#         print(x,'is even mutiple of 2')
+#     elif x % 3 == 0:
+#         print(x, 'is an add mutiple of 3')
+#     else: 
+#         print(x, 'not divisible by 2 and 3')
+
+#3
+# x = int(input('Enter your number: '))
+# import math
+# fakt = math.factorial(x)
+# print(fakt)
+
+#4  
+# log1 = "First"
+# log2 = ""
+# while log1 != log2 :
+#     log2 = input('Enter your login: ')
+#     if log1 != log2:
+#         print('Your login is incorrect. Please, try again.')
+# print('Hello!')
